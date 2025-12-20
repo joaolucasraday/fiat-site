c16f960f8af5ffcf901b45e29b078ae7a977a60a
joaolucasraday <lucasraday@gmail.com>
2025-12-20 19:56:24 -0300

    Finished mobi car page

--- script-carro.js
+++ script-carro.js
@@ -14,3 +14,25 @@ index = (index + 1) % images.length;
 
 changeBackground();
 setInterval(changeBackground, 5000);
+
+/////////////////////
+
+
+const img = document.getElementById('car-color-img');
+const buttons = document.querySelectorAll('.color-buttons button');
+
+if (img && buttons.length) {
+
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      const color = button.dataset.color;
+      const image = img.dataset[color];
+
+      if (image) {
+        img.src = image;
+        img.alt = `Carro na cor ${color}`;
+      }
+    });
+  });
+
+}
