8b4365f8041305d16b8e7e200c75a1942f7d770b
joaolucasraday <lucasraday@gmail.com>
2025-12-20 21:24:41 -0300

    Fix car page css bugs

--- script-carro.js
+++ script-carro.js
@@ -1,19 +1,18 @@
-const images = [
-'imagens/fotos-especiais/mobi-interior.jpg',
-'imagens/fotos-especiais/mobi-farois.jpg',
-'imagens/fotos-especiais/mobi-traseira.jpg'
-];
-
-let index = 0;
 const hero = document.getElementById('imgs-carro');
 
-function changeBackground() {
-hero.style.backgroundImage = `url(${images[index]})`;
-index = (index + 1) % images.length;
-}
+if (hero) {
+  const images = hero.dataset.images.split(',');
 
-changeBackground();
-setInterval(changeBackground, 5000);
+  let index = 0;
+
+  function changeBackground() {
+    hero.style.backgroundImage = `url(${images[index]})`;
+    index = (index + 1) % images.length;
+  }
+
+  changeBackground();
+  setInterval(changeBackground, 5000);
+}
 
 /////////////////////
 
