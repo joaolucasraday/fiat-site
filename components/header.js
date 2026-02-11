01046927f4271166a912f8047b4bae1728b0ee46
João Lucas Raday <lucasraday@gmail.com>
2026-02-11 09:27:02 -0300

    Add files via upload

--- components/header.js
+++ components/header.js
@@ -1,14 +1,18 @@
 function aplicarHeader() {
     const headerContainer = document.getElementById("header");
-    if (!headerContainer) {
-        console.error("Não encontrei o elemento com ID 'header'");
-        return;
-        }
     headerContainer.innerHTML = `
     <header>
         <div class="imagens-header">
-            <a href="index.html" target="_self"><img src="imagens/fiat-logo-white.png" alt="logo-fiat-branco" class="logo"></a>
+            <a href="index.html" target="_self">
+                <img src="imagens/fiat-logo-white.png" alt="logo-fiat-branco" class="logo">
+            </a>
             <img src="imagens/fiat-logo-italia.png" alt="fiat-logo-italia" class="italia">
+            </div>
+        <h1 id="titulo"></h1>
+        <div class="hamburguer" id="hamburguer">
+            <span></span>
+            <span></span>
+            <span></span>
         </div>
         <nav>
             <a href="index.html" target="_self">Home</a>
@@ -22,6 +26,9 @@ function aplicarHeader() {
             <p>(12)1 1234-5678</p>
         </div>
     </header>`;
+
+    const nomePagina = document.body.getAttribute("data-pagina");
+    document.getElementById("titulo").innerText = nomePagina;
 }
 
 document.addEventListener('DOMContentLoaded', aplicarHeader);
